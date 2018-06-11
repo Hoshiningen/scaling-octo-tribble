8de0f10d39415d31de9069bf090ba296fcec2e7b
Matt Stone <mattstone22133@gmail.com>
2018-06-11 08:20:00 -0400

    fixed bug in move_files.bat that didn't move dlls to Debug folder.

--- VS_Solution/scaling-octo-tribble/move_files.bat
+++ VS_Solution/scaling-octo-tribble/move_files.bat
@@ -1,10 +1,9 @@
 @echo off
 echo Hello, I'm going to automatically move the .dll files and the configuration file (.suo) to the appropriate places.
-echo If you ran the visual studio solution before this script, you may need to manually delete the .suo file. The path will be provided.
 
 echo ------------------------------------------------------------------------------------------------------------------------
 echo Moving dll files to Debug and Release folders so the .exe will be able to load them at runtime.
-xcopy /Y /s ".\external_libraries\dlls\*.dll" ".\Release\"
+xcopy /Y /s ".\external_libraries\dlls\*.dll" ".\Debug\"
 xcopy /Y /s ".\external_libraries\dlls\*.dll" ".\Release\"
 
 echo ------------------------------------------------------------------------------------------------------------------------
